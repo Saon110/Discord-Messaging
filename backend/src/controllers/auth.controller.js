891949c160ba60da7f2accac47fb2741b0d1a566
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2026-03-20 10:53:06 +0600

    input validation for user registration

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,5 +2,18 @@ const authService = require('../services/auth.service');
 const { validateRegisterInput } = require('../utils/authValidation');
 
 exports.register = async (req, res, next) => {
+  try {
+    const { isValid, errors, value } = validateRegisterInput(req.body);
 
+    if (!isValid) {
+      return res.status(400).json({
+        success: false,
+        message: 'Validation failed',
+        errors,
+      });
+    }
+
+  } catch (error) {
+    return next(error);
+  }
 };
